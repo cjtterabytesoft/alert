6cc8199133616d6f9c68e2d78fac88998ba7698d
tecnoterabyte <cjtterabytellc@gmail.com>
2019-05-06 13:43:37 +0000

    Apply fixes from StyleCI

--- src/Alert.php
+++ src/Alert.php
@@ -23,49 +23,49 @@ namespace Terabytesoft\Widgets;
  */
 class Alert extends \Yiisoft\Yii\Bootstrap4\Widget
 {
-	/**
-	 * @var array the alert types configuration for the flash messages.
-	 * This array is setup as $key => $value, where:
-	 * - $key is the name of the session flash variable
-	 * - $value is the bootstrap alert type (i.e. danger, success, info, warning)
-	 **/
-	public $alertTypes = [
-		'danger' => 'alert-danger',
-		'dark' => 'alert-dark',
-		'info' => 'alert-info',
-		'light' => 'alert-light',
-		'primary' => 'alert-primary',
-		'secondary' => 'alert-secondary',
-		'success' => 'alert-success',
-		'warning' => 'alert-warning',
-	];
+    /**
+     * @var array the alert types configuration for the flash messages.
+     * This array is setup as $key => $value, where:
+     * - $key is the name of the session flash variable
+     * - $value is the bootstrap alert type (i.e. danger, success, info, warning)
+     **/
+    public $alertTypes = [
+        'danger' => 'alert-danger',
+        'dark' => 'alert-dark',
+        'info' => 'alert-info',
+        'light' => 'alert-light',
+        'primary' => 'alert-primary',
+        'secondary' => 'alert-secondary',
+        'success' => 'alert-success',
+        'warning' => 'alert-warning',
+    ];
 
-	/**
-	 * @var array the options for rendering the close button tag.
-	 **/
-	public $closeButton = [];
+    /**
+     * @var array the options for rendering the close button tag.
+     **/
+    public $closeButton = [];
 
-	public function run()
-	{
-		$session = $this->app->getSession();
-		$flashes = $session->getAllFlashes();
+    public function run()
+    {
+        $session = $this->app->getSession();
+        $flashes = $session->getAllFlashes();
 
-		foreach ($flashes as $type => $data) {
-			if (isset($this->alertTypes[$type])) {
-				$data = (array) $data;
-				foreach ($data as $message) {
-					/* initialize css class for each alert box */
-					$this->options['class'] = $this->alertTypes[$type];
-					/* assign unique id to each alert box */
-					$this->options['id'] = $this->getId() . '-' . $type;
-					echo \yii\bootstrap4\Alert::widget([
-						'body' => $message,
-						'closeButton' => $this->closeButton,
-						'options' => $this->options,
-					]);
-				}
-				$session->removeFlash($type);
-			}
-		}
-	}
+        foreach ($flashes as $type => $data) {
+            if (isset($this->alertTypes[$type])) {
+                $data = (array) $data;
+                foreach ($data as $message) {
+                    /* initialize css class for each alert box */
+                    $this->options['class'] = $this->alertTypes[$type];
+                    /* assign unique id to each alert box */
+                    $this->options['id'] = $this->getId() . '-' . $type;
+                    echo \yii\bootstrap4\Alert::widget([
+                        'body' => $message,
+                        'closeButton' => $this->closeButton,
+                        'options' => $this->options,
+                    ]);
+                }
+                $session->removeFlash($type);
+            }
+        }
+    }
 }
